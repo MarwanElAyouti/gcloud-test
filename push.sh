e664ae2ccd1456451d99d028117a4f66abb274a8
Marwan <marwan@boloo.co>
2023-07-29 05:02:13 +0200

    update push script

--- push.sh
+++ push.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 TARGET_SERVICE=$1
-SERVICE_NAME="marwanelayouti/friendlyeats-frontend"
-DOCKERFILE_PATH="./services/friendlyeats-frontend"
 if [ "$TARGET_SERVICE" = "server" ]; then
     SERVICE_NAME="marwanelayouti/friendlyeats-server"
     DOCKERFILE_PATH="./services/friendlyeats-server"
 fi
 if [ "$TARGET_SERVICE" = "frontend" ]; then
-    SERVICE_NAME="marwanelayouti/friendlyeats-server"
-    DOCKERFILE_PATH="./services/friendlyeats-server"
+    SERVICE_NAME="marwanelayouti/friendlyeats-frontend"
+    DOCKERFILE_PATH="./services/friendlyeats-frontend"
 fi
 
 docker build  --platform linux/amd64 "$DOCKERFILE_PATH" -t "$SERVICE_NAME"
